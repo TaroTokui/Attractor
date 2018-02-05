1e2f63245202fc9fe89f1fe863b09d08d119f67e
tokui <tokui@1-10.com>
2018-02-05 20:15:52 +0900

    first commit

---
+++ Assets/MillionPoints.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class MillionPoints : MonoBehaviour {
+
+    // ==============================
+    #region // Defines
+        
+    const int ThreadBlockSize = 256;
+
+    struct ParticleData
+    {
+        public Vector3 Position;
+        public Vector3 Velocity;
+        public Vector3 Albedo;
+    }
+
+    #endregion // Defines
+
+    // --------------------------------------------------
+    #region // Serialize Fields
+
+    [SerializeField]
+    int _particleCount = 1000000;
+
+    [SerializeField]
+    [Range(-Mathf.PI, Mathf.PI)]
+    float _phi = Mathf.PI;
+
+    [SerializeField]
+    ComputeShader _ComputeShader;
+    
+    [SerializeField]
+    Material _material;
+
+    [SerializeField]
+    Vector3 _MeshScale = new Vector3(1f, 1f, 1f);
+
+    /// 表示領域の中心座標
+    [SerializeField]
+    Vector3 _BoundCenter = Vector3.zero;
+    
+    /// 表示領域のサイズ
+    [SerializeField]
+    Vector3 _BoundSize = new Vector3(300f, 300f, 300f);
+
+    #endregion // Serialize Fields
+
+    // --------------------------------------------------
+    #region // Private Fields
+
+    ComputeBuffer _ParticleDataBuffer;
+    
+    /// GPU Instancingの為の引数
+    uint[] _GPUInstancingArgs = new uint[5] { 0, 0, 0, 0, 0 };
+    
+    /// GPU Instancingの為の引数バッファ
+    ComputeBuffer _GPUInstancingArgsBuffer;
+
+    // point for particle
+    Mesh _pointMesh;
+
+    Vector3 mousePos;
+    float attraction;
+    new Camera camera;
+
+    #endregion // Private Fields
+
+    // --------------------------------------------------
+    #region // MonoBehaviour Methods
+
+    void Awake()
+    {
+        Application.targetFrameRate = 90;
+        QualitySettings.vSyncCount = 0;
+    }
+
+    void Start()
+    {
+        // バッファ生成
+        _ParticleDataBuffer = new ComputeBuffer(_particleCount, Marshal.SizeOf(typeof(ParticleData)));
+        _GPUInstancingArgsBuffer = new ComputeBuffer(1, _GPUInstancingArgs.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        var particleDataArr = new ParticleData[_particleCount];
+
+        // 初期化
+        int kernelId = _ComputeShader.FindKernel("Init");
+        //_ComputeShader.SetInt("_Width", _instanceCountX);
+        //_ComputeShader.SetInt("_Height", _instanceCountY);
+        _ComputeShader.SetBuffer(kernelId, "_ParticleDataBuffer", _ParticleDataBuffer);
+        _ComputeShader.Dispatch(kernelId, (Mathf.CeilToInt(_particleCount / ThreadBlockSize) + 1), 1, 1);
+        
+        // creat point mesh
+        _pointMesh = new Mesh();
+        _pointMesh.vertices = new Vector3[] {
+            new Vector3 (0, 0),
+        };
+        _pointMesh.normals = new Vector3[] {
+            new Vector3 (0, 1, 0),
+        };
+        _pointMesh.SetIndices(new int[] { 0 }, MeshTopology.Points, 0);
+
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 5f;
+        mousePos = camera.ScreenToWorldPoint(mousePos);
+        attraction = Input.GetMouseButton(0) ? 0.1f : 0f;
+
+        // ComputeShader
+        int kernelId = _ComputeShader.FindKernel("Update");
+        _ComputeShader.SetFloat("_time", Time.time / 5.0f);
+        _ComputeShader.SetVector("mousePos", mousePos);
+        _ComputeShader.SetFloat("attraction", attraction);
+        _ComputeShader.SetBuffer(kernelId, "_ParticleDataBuffer", _ParticleDataBuffer);
+        _ComputeShader.Dispatch(kernelId, (Mathf.CeilToInt(_particleCount / ThreadBlockSize) + 1), 1, 1);
+        
+        // GPU Instaicing
+        _GPUInstancingArgs[0] = (_pointMesh != null) ? _pointMesh.GetIndexCount(0) : 0;
+        _GPUInstancingArgs[1] = (uint)_particleCount;
+        _GPUInstancingArgsBuffer.SetData(_GPUInstancingArgs);
+        _material.SetBuffer("_ParticleDataBuffer", _ParticleDataBuffer);
+        _material.SetVector("_MeshScale", _MeshScale);
+        Graphics.DrawMeshInstancedIndirect(_pointMesh, 0, _material, new Bounds(_BoundCenter, _BoundSize), _GPUInstancingArgsBuffer);
+    }
+
+    void OnDestroy()
+    {
+        if (_ParticleDataBuffer != null)
+        {
+            _ParticleDataBuffer.Release();
+            _ParticleDataBuffer = null;
+        }
+        if (_GPUInstancingArgsBuffer != null)
+        {
+            _GPUInstancingArgsBuffer.Release();
+            _GPUInstancingArgsBuffer = null;
+        }
+    }
+    
+    #endregion // MonoBehaviour Method
+}
---
+++ Assets/MillionPoints.shader
@@ -0,0 +1,91 @@
+﻿Shader "Custom/MillionPoints"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1, 1, 1, 1)
+		_Smoothness("Smoothness", Range(0, 1)) = 0
+		_Metallic("Metallic", Range(0, 1)) = 0
+	}
+	SubShader
+	{
+		Tags{ "RenderType" = "Opaque" }
+
+		//Cull Off
+
+		CGPROGRAM
+
+		#pragma surface surf Standard vertex:vert addshadow
+		#pragma instancing_options procedural:setup
+		#pragma target 3.5
+
+		#if SHADER_TARGET >= 35 && (defined(SHADER_API_D3D11) || defined(SHADER_API_GLES3) || defined(SHADER_API_GLCORE) || defined(SHADER_API_XBOXONE) || defined(SHADER_API_PSSL) || defined(SHADER_API_SWITCH) || defined(SHADER_API_VULKAN) || (defined(SHADER_API_METAL) && defined(UNITY_COMPILER_HLSLCC)))
+		#define SUPPORT_STRUCTUREDBUFFER
+		#endif
+
+		#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED) && defined(SUPPORT_STRUCTUREDBUFFER)
+		#define ENABLE_INSTANCING
+		#endif
+
+		struct appdata
+		{
+			float4 vertex : POSITION;
+			float3 normal : NORMAL;
+			float4 tangent : TANGENT;
+			float4 texcoord1 : TEXCOORD1;
+			float4 texcoord2 : TEXCOORD2;
+			uint vid : SV_VertexID;
+			fixed4 color : COLOR;
+			UNITY_VERTEX_INPUT_INSTANCE_ID
+		};
+
+		struct ParticleData
+		{
+			float3 Position;
+			float3 Velocity;
+			float3 Albedo;
+		};
+
+		struct Input
+		{
+			float vface : VFACE;
+			fixed4 color : COLOR;
+		};
+
+		half4 _Color;
+		half _Smoothness;
+		half _Metallic;
+
+		float3 _MeshScale;
+
+		#if defined(ENABLE_INSTANCING)
+		StructuredBuffer<ParticleData> _ParticleDataBuffer;
+		#endif
+
+		void vert(inout appdata v)
+		{
+			#if defined(ENABLE_INSTANCING)
+			// スケールと位置(平行移動)を適用
+			float4x4 matrix_ = (float4x4)0;
+			matrix_._11_22_33_44 = float4(_MeshScale.xyz, 1.0);
+			matrix_._14_24_34 += _ParticleDataBuffer[unity_InstanceID].Position;
+			v.vertex = mul(matrix_, v.vertex);
+			v.color = fixed4(_ParticleDataBuffer[unity_InstanceID].Albedo, 1);
+			#endif
+		}
+
+		void setup()
+		{
+		}
+
+		void surf(Input IN, inout SurfaceOutputStandard o)
+		{
+			o.Albedo = IN.color.rgb * _Color.rgb;
+			o.Metallic = _Metallic;
+			o.Smoothness = _Smoothness;
+			o.Normal = float3(0, 0, IN.vface < 0 ? -1 : 1);
+		}
+
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
